eaf430e608026a3192a19d3505e7afcfca80888e
primeuser <sigdelananda@live.com>
2021-05-30 10:24:28 +0545

    product list and detail page with add to cart button and react route implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,24 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomePage  from './components/pages/HomePage'
+import ProductPage  from './components/pages/ProductPage'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-        <h1>Hello World From React </h1>
-        <HomePage />
+        {/* <h1>Hello World From React </h1> */}
+        {/* <HomePage /> */}
+        <Route path='/' component={HomePage} exact/>
+        <Route path='/product/:id' component={ProductPage} />
         </Container>
         
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
  
