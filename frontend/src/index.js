d4d2726684a3e7b1294d48cdb4de0b7dee5414ee
primeuser <sigdelananda@live.com>
2021-05-31 09:53:13 +0545

    redux store created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,14 @@ import './bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux'
+import store from './store'
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store= {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
