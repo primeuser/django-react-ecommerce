c23ac659c6279e1b1733ca66fc9c7e0efdd92df2
primeuser <sigdelananda@live.com>
2021-05-31 11:19:22 +0545

    loader and message animation

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -6,13 +6,13 @@ import {
 }
 from '../constants/productConstants'
 
-const listProducts = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
     try{
         dispatch({type:PRODUCT_LIST_REQUEST})
 
         const {data}  = await axios.get('/api/products/')
         dispatch({
-            type:PRODUCT_LIST_REQUEST,
+            type:PRODUCT_LIST_SUCCESS,
             payload:data
         })
 
@@ -20,7 +20,7 @@ const listProducts = () => async (dispatch) => {
     catch (error) {
     dispatch({
         type:PRODUCT_LIST_FAIL,
-        payload:error.response && error.response.data.message ? error.response.data.message : error.message
+        payload: error.response && error.response.data.message ? error.response.data.message : error.message,
     })
 }
 
--- frontend/src/components/pages/HomePage.js
+++ frontend/src/components/pages/HomePage.js
@@ -1,34 +1,55 @@
 import React, { useState, useEffect } from 'react'
 // import products from './products'
-import axios from 'axios'
+// import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+import  { listProducts } from '../../actions/productActions'
 
 import {Row, Col} from 'react-bootstrap'
 import Product from '../Product'
+import Loader from '../Loader'
+import Message from '../Message'
 function HomePage() {
-    const [products, setProducts] = useState([])
+    // const [products, setProducts] = useState([])
+
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const  {error, loading, products } = productList
 
     useEffect(() => {
 
-        async function fetchProducts(){
-            const {data}  = await axios.get('/api/products/')
-            setProducts(data)
+        // async function fetchProducts(){
+        //     const {data}  = await axios.get('/api/products/')
+        //     setProducts(data)
 
-        }  
-        fetchProducts()
+        // }  
+        // fetchProducts()
+        dispatch(listProducts())
+
+
+
     }, 
-    []
+    [dispatch]
     )
+
+    // const products = []
 
     return (
         <div>
             <h1>Our Products</h1>
-            <Row>
+            { loading ? <Loader/>
+                : error ? <Message variant='danger'> {error} </Message>  
+                :  
+                <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                         <Product product={product} />
                     </Col>
                 ))}
             </Row>
+        
+        
+            }
+            
         </div>
     )
 }
